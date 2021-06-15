69b14059d4298ec3d055afcc34737d82a841d4c5
Davi Costa <deadspace24@hotmail.com>
2021-06-15 12:47:15 -0300

    CRUD Scaffolding

---
+++ SafesWebMvc/Data/SafesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SafesWebMvc.Models;
+
+namespace SafesWebMvc.Data
+{
+    public class SafesWebMvcContext : DbContext
+    {
+        public SafesWebMvcContext (DbContextOptions<SafesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SafesWebMvc.Models.Departament> Departament { get; set; }
+    }
+}
--- SafesWebMvc/Startup.cs
+++ SafesWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SafesWebMvc.Data;
 
 namespace SafesWebMvc {
     public class Startup {
@@ -28,6 +30,9 @@ namespace SafesWebMvc {
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+    services.AddDbContext<SafesWebMvcContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SafesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
