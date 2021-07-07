09485ac7b335184622c82d4685653c74721ba257
Davi Costa <deadspace24@hotmail.com>
2021-07-07 09:50:15 -0300

    Seeding Service

--- SafesWebMvc/Startup.cs
+++ SafesWebMvc/Startup.cs
@@ -28,18 +28,20 @@ namespace SafesWebMvc {
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SafesWebMvcContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SafesWebMvcContext"), builder =>
         builder.MigrationsAssembly("SafesWebMvc")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else {
                 app.UseExceptionHandler("/Home/Error");
