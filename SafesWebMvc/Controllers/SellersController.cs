cd561ab31062ff8ded30223d50313cad8e2da5aa
Davi Costa <davi.costa@ajdesenvolvimento.com.br>
2021-08-09 15:03:08 -0300

    Returning custom error page

--- SafesWebMvc/Controllers/SellersController.cs
+++ SafesWebMvc/Controllers/SellersController.cs
@@ -5,23 +5,29 @@ using SafesWebMvc.Services;
 using SafesWebMvc.Services.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SafesWebMvc.Controllers {
-    public class SellersController : Controller {
+namespace SafesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
         private readonly SellerService _sellerService;
         private readonly DepartamentService _departamentService;
-        public SellersController(SellerService sellerService, DepartamentService departamentService) {
+        public SellersController(SellerService sellerService, DepartamentService departamentService)
+        {
             _sellerService = sellerService;
             _departamentService = departamentService;
         }
-        public IActionResult Index() {
+        public IActionResult Index()
+        {
             var list = _sellerService.FindAll();
             return View(list);
         }
 
-        public IActionResult Create() {
+        public IActionResult Create()
+        {
             var departaments = _departamentService.FindAll();
             var viewModel = new SellerFormViewModel { Departaments = departaments };
             return View(viewModel);
@@ -29,7 +35,8 @@ namespace SafesWebMvc.Controllers {
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller) {
+        public IActionResult Create(Seller seller)
+        {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -38,13 +45,13 @@ namespace SafesWebMvc.Controllers {
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
@@ -62,13 +69,13 @@ namespace SafesWebMvc.Controllers {
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj);
@@ -78,13 +85,13 @@ namespace SafesWebMvc.Controllers {
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
 
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             List<Departament> departments = _departamentService.FindAll();
@@ -99,22 +106,33 @@ namespace SafesWebMvc.Controllers {
         {
             if (id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
-           
+
             try
             {
                 _sellerService.Update(seller);
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException)
+            catch (NotFoundException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch (DbConcurrencyException)
+            catch (DbConcurrencyException e)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
+        }
+
+        public IActionResult Error(string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+
+            return View(viewModel);
         }
     }
 }
--- SafesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SafesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -3,6 +3,7 @@ using System;
 namespace SafesWebMvc.Models.ViewModels {
     public class ErrorViewModel {
         public string RequestId { get; set; }
+        public string Message { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
